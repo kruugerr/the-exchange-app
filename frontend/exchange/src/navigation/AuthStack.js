6deb84227e87368a795c1068ce931777f5fafb1e
kruugerr <zachgould2004@gmail.com>
2025-02-23 14:54:26 -0500

    main navigation stack

---
+++ frontend/exchange/src/navigation/AuthStack.js
@@ -0,0 +1,18 @@
+// src/navigation/AuthStack.js
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStack = () => {
+  return (
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
